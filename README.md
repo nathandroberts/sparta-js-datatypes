682a878634fdb393b5103cb94be72347f6d2bb33
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-05 22:24:50 +0000

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sparta-js-datatypes
+Repo created to understand the different data types used in javascript.
--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-console.log('hello');
+// console.log('hello');
+// var a = 'this is a string';
+// var b = 'this is another string'
+//
+// var array = [a, b];
+//
+// var combinedString = a + ' ' + b;
+//
+//
+// console.log(combinedString);
+// console.log(a.indexOf("h"));
+// console.log(a.substr(2,4));
+
+
+ // var myNumber = 5;
+ // var myDecimal = 5.5;
+ // var myExponential = 123e5;
+ //
+ // console.log(myNumber + 3);
+ // console.log(myNumber - 3);
+ // console.log(myNumber * 3);
+ // console.log(myNumber / 5);
+ //
+ // console.log(Number.isFinite(myNumber));
+ // console.log(myNumber.toString());
+ //
+ // var myBoolean = true;
+ // console.log(myBoolean);
+ // myBoolean = false;
+ // console.log(myBoolean);
+ //
+ // var myArray = [1, 2, 3, 'four', 'five', 'six']
+//
+// console.log(myArray);
+//
+// console.log(myArray[myArray.length -1]);
+//
+// var join = myArray + ['oneThing','anotherThing'];
+// console.log(join);
+// console.log(typeof join);
+// myArray.push('where do i go?')
+// console.log(myArray)
+// myArray.pop()
+// console.log(myArray);
+// console.log(myArray.reverse());
+
+var myString= '5';
+var myDecimalString = '2.5'
+var myNumber = 3;
+console.log(myString+myNumber);
+console.log(parseInt(myString)+ myNumber);
+console.log(parseFloat(myDecimalString)+myNumber);
+console.log(parseFloat(myDecimalString)+ myNumber);
